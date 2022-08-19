870aec06653f6b28eef13b521a009c611ecd64ec
kafeeroemmanuel <emzpipi@gmail.com>
2022-08-19 14:30:01 +0300

    Finishing Arrays

--- app.js
+++ app.js
@@ -172,3 +172,81 @@
 const nameData = ["Ema", "Kafeero", "Mr", 45];
 const [firstName, lastName, ...otherData] = nameData;
 console.log(firstName, lastName, otherData);
+
+// const numbers = [1, 5, 3, 5, 87, 9, 11];
+
+// const bigNumbs = numbers.filter((num) => num > 5);
+// console.log(bigNumbs);
+
+// const mapNum = numbers.map((item) => {
+//   const numObj = { num: item };
+//   return numObj;
+// });
+// console.log(mapNum);
+
+// const redNum = numbers.reduce((prevNum, currNum) => {
+//   return prevNum * currNum;
+// }, 1);
+// console.log(redNum);
+
+// const findMax = () => {
+//   // const [big, small] = numbers
+//   const big = Math.max(...numbers);
+//   //   return [big];
+//   const small = Math.min(...numbers);
+//   return [big, small];
+// };
+
+// console.log(findMax());
+
+const numbers = [1, -22, 100, 4, 5, 6];
+
+const numsGreater5 = numbers.filter((val) => val > 5);
+console.log(numsGreater5);
+
+const mappedNumbers = numbers.map((val) => ({ num: val }));
+console.log(mappedNumbers);
+
+const multiplication = numbers.reduce(
+  (curResult, curValue) => curResult * curValue,
+  1
+);
+console.log(multiplication);
+
+function findMax(...nums) {
+  let curMax = nums[0];
+  for (const num of nums) {
+    if (num > curMax) {
+      curMax = num;
+    }
+  }
+  return curMax;
+}
+
+console.log(findMax(...numbers));
+
+function findMinMax(...nums) {
+  let curMax = nums[0];
+  let curMin = nums[0];
+  for (const num of nums) {
+    if (num > curMax) {
+      curMax = num;
+    }
+    if (num < curMin) {
+      curMin = num;
+    }
+  }
+  return [curMin, curMax];
+}
+
+const [min, max] = findMinMax(...numbers);
+
+console.log(min, max);
+
+// set is a data structure for ensuring uniqueness, the second -5 would be ignored
+const userIds = new Set();
+userIds.add(10);
+userIds.add(-5);
+userIds.add(-5);
+
+console.log(userIds);
